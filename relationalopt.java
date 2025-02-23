83e9377d82a93b29cf9dbf8a00d2fe08a335f5c6
yashwanth1705 <yashwanthrathnam746@gmail.com>
2025-02-23 15:31:06 +0530

    Add Java classes for basic programming concepts: Hello World, data types, literals, type conversion, and variable operations

---
+++ relationalopt.java
@@ -0,0 +1,20 @@
+public class relationalopt {
+    public static void main(String[] args) {
+        //< less than
+        //> greater than 
+        // == equal to
+        // != not equal to
+        // <= less than or equal to
+        // >= greater than or equal to
+        int x = 7;
+        int y = 5;
+        
+        double a = 7.4;
+        float b = 7.4f;
+
+        boolean result = x <= y;
+        System.out.println(result);
+        boolean result2 = a == b;
+        System.out.println(result2);
+    }
+}
